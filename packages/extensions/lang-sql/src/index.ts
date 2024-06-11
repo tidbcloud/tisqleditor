1094cc365882e3daa2d3855bd8d014b83ab2fb83
sanshuiyijing <jingdeveloper@126.com>
2024-06-11 17:55:30 +0800

    feat: extensions support monorepo

--- packages/extensions/lang-sql/src/index.ts
+++ packages/extensions/lang-sql/src/index.ts
@@ -1,6 +1,7 @@
 import { MySQL, sql, SQLConfig } from '@codemirror/lang-sql'
+import { LanguageSupport } from '@codemirror/language'
 
-export const langSql = (config: SQLConfig) =>
+export const langSql = (config: SQLConfig): LanguageSupport =>
   sql({
     dialect: MySQL,
     upperCaseKeywords: true,
