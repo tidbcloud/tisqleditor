29d8d0c1d1bffbfecaaf84fd2f97413b5c0d69db
baurine <2008.hbl@gmail.com>
2024-06-17 18:27:32 +0800

    feat(playground): refine del and rename file

--- packages/playground/src/components/biz/files-list.tsx
+++ packages/playground/src/components/biz/files-list.tsx
@@ -1,17 +1,177 @@
+import { useEffect, useMemo, useState } from 'react'
 import {
   MinusIcon,
   Pencil1Icon,
   PlusIcon,
   ReloadIcon
 } from '@radix-ui/react-icons'
-
 import dayjs from 'dayjs'
 import { useMutation, useQuery } from '@tanstack/react-query'
-import { useEffect } from 'react'
 
 import { IFile, useFilesContext } from '@/contexts/files-context'
 import { Button } from '@/components/ui/button'
 import { Skeleton } from '@/components/ui/skeleton'
+import {
+  AlertDialog,
+  AlertDialogAction,
+  AlertDialogCancel,
+  AlertDialogContent,
+  AlertDialogDescription,
+  AlertDialogFooter,
+  AlertDialogHeader,
+  AlertDialogTitle,
+  AlertDialogTrigger
+} from '@/components/ui/alert-dialog'
+import {
+  Dialog,
+  DialogClose,
+  DialogContent,
+  DialogFooter,
+  DialogHeader,
+  DialogTitle,
+  DialogTrigger
+} from '@/components/ui/dialog'
+import { Input } from '@/components/ui/input'
+
+function DelFileAlertDialog() {
+  const {
+    state: { allFiles, activeFileId, setActiveFileId },
+    api: { loadFiles, delFile }
+  } = useFilesContext()
+
+  const fileName = useMemo(
+    () => allFiles.find((f) => f.id === activeFileId)?.name ?? '',
+    [allFiles, activeFileId]
+  )
+
+  const { refetch: refetchFiles } = useQuery({
+    queryKey: ['files_list'],
+    queryFn: loadFiles
+  })
+
+  async function handleDeleteFile() {
+    if (!activeFileId) {
+      return
+    }
+    await delFile(activeFileId)
+    await refetchFiles()
+    setActiveFileId(null)
+  }
+
+  const delFileMut = useMutation({
+    mutationFn: handleDeleteFile
+  })
+
+  return (
+    <AlertDialog>
+      <AlertDialogTrigger asChild>
+        <Button
+          variant="outline"
+          size="icon"
+          className="h-8 w-8"
+          disabled={!activeFileId || delFileMut.isPending}
+        >
+          {delFileMut.isPending ? (
+            <ReloadIcon className="h-4 w-4 animate-spin" />
+          ) : (
+            <MinusIcon className="h-4 w-4" />
+          )}
+        </Button>
+      </AlertDialogTrigger>
+      <AlertDialogContent>
+        <AlertDialogHeader>
+          <AlertDialogTitle>
+            Are you sure to delete {fileName}?
+          </AlertDialogTitle>
+          <AlertDialogDescription>
+            This action cannot be undone. This will permanently delete your SQL
+            file and remove your data from our servers.
+          </AlertDialogDescription>
+        </AlertDialogHeader>
+        <AlertDialogFooter>
+          <AlertDialogCancel>Cancel</AlertDialogCancel>
+          <AlertDialogAction onClick={() => delFileMut.mutate()}>
+            Continue
+          </AlertDialogAction>
+        </AlertDialogFooter>
+      </AlertDialogContent>
+    </AlertDialog>
+  )
+}
+
+function RenameFileDialog() {
+  const {
+    state: { allFiles, activeFileId },
+    api: { loadFiles, renameFile }
+  } = useFilesContext()
+
+  const oriFileName = useMemo(
+    () => allFiles.find((f) => f.id === activeFileId)?.name ?? '',
+    [allFiles, activeFileId]
+  )
+  const [newFileName, setNewFileName] = useState('')
+  useEffect(() => {
+    setNewFileName(oriFileName)
+  }, [oriFileName])
+
+  const { refetch: refetchFiles } = useQuery({
+    queryKey: ['files_list'],
+    queryFn: loadFiles
+  })
+
+  async function handleRenameFile() {
+    if (!activeFileId || newFileName === oriFileName) {
+      return
+    }
+    await renameFile(activeFileId, newFileName)
+    await refetchFiles()
+  }
+
+  const renameFileMut = useMutation({
+    mutationFn: handleRenameFile
+  })
+
+  return (
+    <Dialog>
+      <DialogTrigger asChild>
+        <Button
+          variant="outline"
+          size="icon"
+          className="h-8 w-8"
+          disabled={!activeFileId || renameFileMut.isPending}
+        >
+          {renameFileMut.isPending ? (
+            <ReloadIcon className="h-4 w-4 animate-spin" />
+          ) : (
+            <Pencil1Icon className="h-4 w-4" />
+          )}
+        </Button>
+      </DialogTrigger>
+      <DialogContent className="sm:max-w-[425px]">
+        <DialogHeader>
+          <DialogTitle>Edit SQL file name</DialogTitle>
+        </DialogHeader>
+        <div className="py-2">
+          <Input
+            value={newFileName}
+            onChange={(ev) => setNewFileName(ev.target.value)}
+          />
+        </div>
+        <DialogFooter>
+          <DialogClose asChild>
+            <Button
+              type="submit"
+              disabled={!newFileName}
+              onClick={() => renameFileMut.mutate()}
+            >
+              Save changes
+            </Button>
+          </DialogClose>
+        </DialogFooter>
+      </DialogContent>
+    </Dialog>
+  )
+}
 
 export default function FilesList() {
   const {
@@ -23,7 +183,7 @@ export default function FilesList() {
       activeFileId,
       setActiveFileId
     },
-    api: { loadFiles, openFile, addFile, delFile }
+    api: { loadFiles, openFile, addFile }
   } = useFilesContext()
 
   const {
@@ -40,9 +200,6 @@ export default function FilesList() {
 
   const addFileMut = useMutation({
     mutationFn: handleAddFile
-  })
-  const delFileMut = useMutation({
-    mutationFn: handleDeleteFile
   })
 
   async function handleOpenFile(file: IFile) {
@@ -69,19 +226,6 @@ export default function FilesList() {
     await refetchFiles()
   }
 
-  async function handleDeleteFile() {
-    if (!activeFileId) {
-      return
-    }
-    await delFile(activeFileId)
-    await refetchFiles()
-    setActiveFileId(null)
-  }
-
-  async function handleRenameFile() {
-    // TODO
-  }
-
   return (
     <div>
       <div className="flex gap-2">
@@ -98,28 +242,9 @@ export default function FilesList() {
             <PlusIcon className="h-4 w-4" />
           )}
         </Button>
-        <Button
-          variant="outline"
-          size="icon"
-          className="h-8 w-8"
-          onClick={handleRenameFile}
-          disabled={!activeFileId}
-        >
-          <Pencil1Icon className="h-4 w-4" />
-        </Button>
-        <Button
-          variant="outline"
-          size="icon"
-          className="h-8 w-8"
-          onClick={() => delFileMut.mutate()}
-          disabled={!activeFileId || delFileMut.isPending}
-        >
-          {delFileMut.isPending ? (
-            <ReloadIcon className="h-4 w-4 animate-spin" />
-          ) : (
-            <MinusIcon className="h-4 w-4" />
-          )}
-        </Button>
+
+        <RenameFileDialog />
+        <DelFileAlertDialog />
       </div>
 
       <ul className="mt-4 space-y-1">
