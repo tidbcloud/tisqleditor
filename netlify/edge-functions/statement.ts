4199d0a6d695a11bfb017c9fe27c851b74a6c2fe
baurine <2008.hbl@gmail.com>
2024-06-20 13:03:01 +0800

    feat(playground): refine statement api

--- netlify/edge-functions/statement.ts
+++ netlify/edge-functions/statement.ts
@@ -1,23 +1,36 @@
+import type { Config, Context } from '@netlify/edge-functions'
 import { connect } from 'https://esm.sh/@tidbcloud/serverless'
 
-export default async () => {
+type StatementReq = {
+  database: string
+  sql: string
+}
+
+export default async (req: Request, _context: Context) => {
+  const body: StatementReq = await req.json()
+
   const conn = connect({
     url: Netlify.env.get('TIDBCLOUD_DATABASE_URL')
   })
 
-  const res = await conn.execute(
-    `
-SELECT g.*
-FROM game_genre gg
-LEFT JOIN games g ON g.app_id = gg.app_id
-WHERE gg.genre_id  = 9
-ORDER BY g.estimated_owners DESC
-LIMIT 10`,
-    null,
-    { arrayMode: true, fullResult: true }
-  )
+  if (body.database) {
+    await conn.execute(`use ?`, [body.database])
+  }
+
+  //   const res = await conn.execute(`
+  // SELECT g.*
+  // FROM game_genre gg
+  // LEFT JOIN games g ON g.app_id = gg.app_id
+  // WHERE gg.genre_id  = 9
+  // ORDER BY g.estimated_owners DESC
+  // LIMIT 10`)
+
+  const res = await conn.execute(body.sql, null, {
+    arrayMode: true,
+    fullResult: true
+  })
 
   return new Response(JSON.stringify({ code: 200, message: 'ok', data: res }))
 }
 
-export const config = { path: '/api/statement' }
+export const config: Config = { path: '/api/statement' }
