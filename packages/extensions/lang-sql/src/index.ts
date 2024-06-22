258db2c22ede32574969c5d62a79963c60b42260
baurine <2008.hbl@gmail.com>
2024-06-22 21:13:10 +0800

    chore: remove lang-sql extension

--- packages/extensions/lang-sql/src/index.ts
+++
@@ -1,9 +0,0 @@
-import { MySQL, sql, SQLConfig } from '@codemirror/lang-sql'
-import { LanguageSupport } from '@codemirror/language'
-
-export const langSql = (config: SQLConfig): LanguageSupport =>
-  sql({
-    dialect: MySQL,
-    upperCaseKeywords: true,
-    ...config
-  })
