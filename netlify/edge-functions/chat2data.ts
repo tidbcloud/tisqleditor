3add94e1ac5dba490f9f2296ad71485cdfb99b2c
baurine <2008.hbl@gmail.com>
2024-06-27 12:07:41 +0800

    feat(chat2query-api): refine api

--- netlify/edge-functions/chat2data.ts
+++ netlify/edge-functions/chat2data.ts
@@ -4,13 +4,37 @@ const url =
   Netlify.env.get('TIDBCLOUD_CHAT2QUERY_APP_URL_ENDPOINT') + '/v3/chat2data'
 const publicKey = Netlify.env.get('TIDBCLOUD_CHAT2QUERY_APP_PUBLIC_KEY')
 const privateKey = Netlify.env.get('TIDBCLOUD_CHAT2QUERY_APP_PRIVATE_KEY')
+const clusterId = Netlify.env.get('TIDBCLOUD_CHAT2QUERY_CLUSTER_ID')
 
-export default async (_req: Request, _context: Context) => {
-  const data = {
-    cluster_id: '10571975649917075631',
-    database: 'game',
-    question: 'show most 10 popular games'
+type Chat2DataReq = {
+  database: string
+  question: string
+}
+
+export default async (req: Request, _context: Context) => {
+  const body: Chat2DataReq = await req.json()
+
+  if (!body.database || !body.question) {
+    return new Response(
+      JSON.stringify({
+        code: 400,
+        message: 'bad request params, database or question should not empty.',
+        data: {}
+      }),
+      { status: 400 }
+    )
   }
+
+  const data = {
+    cluster_id: clusterId,
+    ...body
+  }
+
+  // const data = {
+  //   cluster_id: clusterId,
+  //   database: 'game',
+  //   question: 'show most 10 popular games'
+  // }
 
   const fetchOptions = {
     method: 'POST',
