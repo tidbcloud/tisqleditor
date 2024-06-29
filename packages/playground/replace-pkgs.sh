32ffe47c2a078f8ac308f2f3930ecf5b97fe2310
baurine <2008.hbl@gmail.com>
2024-06-29 23:32:25 +0800

    chore: try 0.0.3 packages

---
+++ packages/playground/replace-pkgs.sh
@@ -0,0 +1,6 @@
+
+#!/usr/bin/env bash
+
+pnpm remove @tidbcloud/codemirror-extension-ai-widget @tidbcloud/codemirror-extension-sql-autocomplete @tidbcloud/codemirror-extension-cur-sql @tidbcloud/codemirror-extension-cur-sql-gutter @tidbcloud/codemirror-extension-linters @tidbcloud/codemirror-extension-save-helper @tidbcloud/codemirror-extension-sql-parser @tidbcloud/codemirror-extension-themes @tidbcloud/tisqleditor-react
+
+pnpm add @tidbcloud/codemirror-extension-ai-widget @tidbcloud/codemirror-extension-sql-autocomplete @tidbcloud/codemirror-extension-cur-sql @tidbcloud/codemirror-extension-cur-sql-gutter @tidbcloud/codemirror-extension-linters @tidbcloud/codemirror-extension-save-helper @tidbcloud/codemirror-extension-sql-parser @tidbcloud/codemirror-extension-themes @tidbcloud/tisqleditor-react
