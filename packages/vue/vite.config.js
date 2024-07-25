a4092e6b9807219c8dfdfc8f838f1a717f181df4
baurine <2008.hbl@gmail.com>
2024-07-25 22:19:25 +0800

    feat(vue): generate type declaration

--- packages/vue/vite.config.js
+++ packages/vue/vite.config.js
@@ -1,8 +1,9 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
+import dts from 'vite-plugin-dts'
 
 export default defineConfig({
-  plugins: [vue()],
+  plugins: [vue(), dts()],
   build: {
     lib: {
       entry: 'src/index.ts',
