9edb3f00bc497ae84df4f03b7f9246cbd1f3f753
baurine <2008.hbl@gmail.com>
2024-07-25 13:14:25 +0800

    feat(vue): config build lib

--- packages/vue/src/components/HelloWorld.vue
+++ packages/vue/src/components/HelloWorld.vue
@@ -33,9 +33,3 @@ const count = ref(0)
   </p>
   <p class="read-the-docs">Click on the Vite and Vue logos to learn more</p>
 </template>
-
-<style scoped>
-.read-the-docs {
-  color: #888;
-}
-</style>
