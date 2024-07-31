10262d1d318d6ba9eec0ae8b3cab6b5bcf1527ad
baurine <2008.hbl@gmail.com>
2024-07-31 18:04:11 +0800

    chore: set defaultSize for panel

--- packages/playground-nextjs/app/playground/panels.tsx
+++ packages/playground-nextjs/app/playground/panels.tsx
@@ -10,13 +10,13 @@ import { ResultPanel } from './result-panel'
 export function Panels() {
   return (
     <ResizablePanelGroup direction="horizontal">
-      <ResizablePanel defaultSize={15} maxSize={30}>
+      <ResizablePanel defaultSize={20} maxSize={30}>
         <LeftPanel />
       </ResizablePanel>
 
       <ResizableHandle withHandle />
 
-      <ResizablePanel>
+      <ResizablePanel defaultSize={80}>
         <ResizablePanelGroup direction="vertical">
           <ResizablePanel defaultSize={75} minSize={5}>
             <EditorPanel />
