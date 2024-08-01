41c253b9d56a6294e7898bd79e7300126aa5441c
baurine <2008.hbl@gmail.com>
2024-08-01 12:04:26 +0800

    feat(vue-demo): init package

---
+++ packages/vue-demo/README.md
@@ -0,0 +1,14 @@
+# TiSQLEditor Vue Component Demo
+
+This demo is used to test `@tidbcloud/tisqleditor-vue` package, created by Vue3+Typescript+Vite.
+
+## Preview
+
+![tisqleditor vue](./public/tisqleditor-vue.png)
+
+## Get Started
+
+```bash
+pnpm i
+pnpm dev
+```
---
+++ packages/vue-demo/src/App.vue
@@ -0,0 +1,35 @@
+<script setup lang="ts">
+import { ref } from "vue";
+import { SQLEditor, EditorCacheProvide } from "@tidbcloud/tisqleditor-vue";
+import { oneDark, bbedit } from "@tidbcloud/codemirror-extension-themes";
+
+const isDark = ref(true);
+function toggle() {
+  isDark.value = !isDark.value;
+}
+</script>
+
+<template>
+  <EditorCacheProvide>
+    <h2>TiSQLEditor Vue Component Demo</h2>
+    <button @click="toggle">
+      toggle theme: {{ isDark ? "dark" : "light" }}
+    </button>
+
+    <SQLEditor
+      class="editorContainer"
+      editorId="test"
+      doc="select * from test;"
+      :theme="isDark ? oneDark : bbedit"
+    />
+  </EditorCacheProvide>
+</template>
+
+<style scoped>
+.editorContainer {
+  height: 400px;
+  width: 800px;
+  text-align: left;
+  margin-top: 16px;
+}
+</style>
