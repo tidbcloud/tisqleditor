b160106cc9d0bf50df9cc26d2214eeea684ac457
baurine <2008.hbl@gmail.com>
2024-08-01 12:18:03 +0800

    feat(vue-demo): demo more extensions

--- packages/vue-demo/src/App.vue
+++ packages/vue-demo/src/App.vue
@@ -1,19 +1,51 @@
 <script setup lang="ts">
-import { ref } from "vue";
-import { SQLEditor, EditorCacheProvide } from "@tidbcloud/tisqleditor-vue";
-import { oneDark, bbedit } from "@tidbcloud/codemirror-extension-themes";
+import { ref } from 'vue'
 
-const isDark = ref(true);
+import { SQLEditor, EditorCacheProvide } from '@tidbcloud/tisqleditor-vue'
+import { oneDark, bbedit } from '@tidbcloud/codemirror-extension-themes'
+import {
+  aiWidget,
+  isUnifiedMergeViewActive
+} from '@tidbcloud/codemirror-extension-ai-widget'
+import { curSqlGutter } from '@tidbcloud/codemirror-extension-cur-sql-gutter'
+import { sqlAutoCompletion } from '@tidbcloud/codemirror-extension-sql-autocomplete'
+
+import { delay } from './lib/delay'
+
+const isDark = ref(true)
 function toggle() {
-  isDark.value = !isDark.value;
+  isDark.value = !isDark.value
 }
+
+const extraExts = [
+  aiWidget({
+    chat: async () => {
+      await delay(2000)
+      return {
+        status: 'success',
+        message:
+          'select * from test;\n-- the data is mocked, replace by your own api when using'
+      }
+    },
+    cancelChat: () => {},
+    getDbList: () => {
+      return ['test1', 'test2']
+    }
+  }),
+  sqlAutoCompletion(),
+  curSqlGutter({
+    whenHide(view) {
+      return isUnifiedMergeViewActive(view.state)
+    }
+  })
+]
 </script>
 
 <template>
   <EditorCacheProvide>
     <h2>TiSQLEditor Vue Component Demo</h2>
     <button @click="toggle">
-      toggle theme: {{ isDark ? "dark" : "light" }}
+      toggle theme: {{ isDark ? 'dark' : 'light' }}
     </button>
 
     <SQLEditor
@@ -21,6 +53,7 @@ function toggle() {
       editorId="test"
       doc="select * from test;"
       :theme="isDark ? oneDark : bbedit"
+      :extraExts="extraExts"
     />
   </EditorCacheProvide>
 </template>
