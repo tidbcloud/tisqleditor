faec95faa8465d9cc87efbb084ba55472a32f499
sanshuiyijing <jingdeveloper@126.com>
2024-10-10 10:58:02 +0800

    fix: events callback add editor state

--- packages/extensions/events/src/doc-change.ts
+++ packages/extensions/events/src/doc-change.ts
@@ -1,14 +1,14 @@
-import { Extension } from '@codemirror/state'
+import { EditorState, Extension } from '@codemirror/state'
 import { EditorView, ViewUpdate } from '@codemirror/view'
 
-type DocChangeHandler = (view: EditorView, content: string) => void
+type DocChangeHandler = (view: EditorView, state: EditorState, content: string) => void
 
 const docChangeListener = (handler: DocChangeHandler) => {
   return EditorView.updateListener.of((update: ViewUpdate) => {
     if (!update.docChanged) return
 
     const { state } = update.view
-    handler(update.view, state.doc.sliceString(0))
+    handler(update.view, update.state, state.doc.sliceString(0))
   })
 }
 
